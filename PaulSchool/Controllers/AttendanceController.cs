15b6fa938333487cf488dc2b205ec7bc4e88c194
ecnalyr <ecnalyr@gmail.com>
2012-04-27 21:32:11 -0500

    /Attendance/List returns an improperly formatted list of attendance for CourseID == 4
    This is the foundation for future attendance tables.
    
    Signed-off-by: ecnalyr <ecnalyr@gmail.com>

---
+++ PaulSchool/Controllers/AttendanceController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaulSchool.Models;
+using PaulSchool.ViewModels;
+
+namespace PaulSchool.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Attendance/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        //
+        // GET: /Attendance/Attendance/1
+        public ActionResult Attendance(int id)
+        {
+            var viewModel = new AttendanceViewModel
+            {
+                AttendanceItems = db.Attendance.Where(s => s.CourseID == id)
+            };
+            return View(viewModel);
+        }
+
+        //
+        // GET: /Attendance/List  - Should list all attendance values from ONLY courseID = 4
+        public ActionResult List()
+        {
+            IEnumerable<Attendance> model = db.Attendance.Where(s => s.CourseID == 4);
+            return View(model);
+        }
+
+    }
+}
---
+++ PaulSchool/ViewModels/AttendanceViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PaulSchool.Models;
+
+namespace PaulSchool.ViewModels
+{
+    public class AttendanceViewModel
+    {
+        public IEnumerable<Attendance> AttendanceItems { get; set; }
+    }
+}
