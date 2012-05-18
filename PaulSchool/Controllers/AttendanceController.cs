c4115994c0e7a3e96dca06cc3c79d67668ef2714
ecnalyr <ecnalyr@gmail.com>
2012-05-18 06:16:19 -0500

    Student can now see the attendance details of any class in which they are enrolled.
    Signed-off-by: ecnalyr <ecnalyr@gmail.com>

--- PaulSchool/Controllers/AttendanceController.cs
+++ PaulSchool/Controllers/AttendanceController.cs
@@ -49,5 +49,35 @@ namespace PaulSchool.Controllers
             };
             return View(model);
         }
+
+        public ActionResult StudentDetails(int id)
+        {
+            //
+            // Generates list of Attendances specifically for current Course
+            var attendanceItems = db.Attendance.Where(s => s.CourseID == id);
+            List<Attendance> attendanceItemsList = attendanceItems.ToList();
+            // End of generating list of Attendances
+
+            var student = db.Students.Where(a => a.UserName == User.Identity.Name);  // This works for adding one student, not all of them.
+            List<Student> StudentList = student.ToList();
+
+            //
+            // Generates list of AttendingDays specifically for current Course
+            Course course = db.Courses.FirstOrDefault(p => p.CourseID == id);
+            List<int> attDayList = new List<int>();
+            for (int i = 0; i < course.AttendingDays; i++)
+            {
+                attDayList.Add(i + 1);
+            };
+            // End of generating list of AttendingDays
+
+            AttendanceReportViewModel model = new AttendanceReportViewModel
+            {
+                AttendanceDays = attDayList,
+                Students = StudentList, // should be only one student for this section
+                Attendances = attendanceItemsList,
+            };
+            return View(model);
+        }
     }
 }
