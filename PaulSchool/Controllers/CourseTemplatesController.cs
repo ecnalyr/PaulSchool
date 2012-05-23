b83634eceae8f436cafcf168daff33e4e71dc3a4
ecnalyr <ecnalyr@gmail.com>
2012-05-23 09:35:41 -0500

    Added CRUD for Course Templates
    Accessible to Admin by link visible on Course index page.
    
    Signed-off-by: ecnalyr <ecnalyr@gmail.com>

---
+++ PaulSchool/Controllers/CourseTemplatesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaulSchool.Models;
+
+namespace PaulSchool.Controllers
+{ 
+    [Authorize (Roles= "Administrator, SuperAdministrator")]
+    public class CourseTemplatesController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /CourseTemplates/
+
+        public ViewResult Index()
+        {
+            return View(db.CourseTemplates.ToList());
+        }
+
+        //
+        // GET: /CourseTemplates/Details/5
+
+        public ViewResult Details(int id)
+        {
+            CourseTemplates coursetemplates = db.CourseTemplates.Find(id);
+            return View(coursetemplates);
+        }
+
+        //
+        // GET: /CourseTemplates/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /CourseTemplates/Create
+
+        [HttpPost]
+        public ActionResult Create(CourseTemplates coursetemplates)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CourseTemplates.Add(coursetemplates);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(coursetemplates);
+        }
+        
+        //
+        // GET: /CourseTemplates/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            CourseTemplates coursetemplates = db.CourseTemplates.Find(id);
+            return View(coursetemplates);
+        }
+
+        //
+        // POST: /CourseTemplates/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(CourseTemplates coursetemplates)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(coursetemplates).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(coursetemplates);
+        }
+
+        //
+        // GET: /CourseTemplates/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            CourseTemplates coursetemplates = db.CourseTemplates.Find(id);
+            return View(coursetemplates);
+        }
+
+        //
+        // POST: /CourseTemplates/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            CourseTemplates coursetemplates = db.CourseTemplates.Find(id);
+            db.CourseTemplates.Remove(coursetemplates);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- PaulSchool/Models/CourseTemplates.cs
+++ PaulSchool/Models/CourseTemplates.cs
@@ -2,19 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PaulSchool.Models
 {
     public class CourseTemplates
     {
         public int CourseTemplatesID { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Title { get; set; }
+
+        [Required]
+        [Range(1, 100.00, ErrorMessage = "Credits must be a positive value")]
         public int Credits { get; set; }
+
+        [Required]
         public bool Elective { get; set; }
+
+        [Range(1, 100.00, ErrorMessage = "Total Attending Days must be a positive value (often 10, 8, or 3)")]
         public int AttendingDays { get; set; } // 10, 8, 3, or custom
+
+        [Range(1, 100.00, ErrorMessage = "Attendance Cap must be a positive value (typically 30)")]
         public int AttendanceCap { get; set; } // default of 30
+
         public string Location { get; set; }
+
         public string Parish { get; set; }
+
         public string Description { get; set; }
     }
 }
