3bd78f58fedfddf58d009808727d0f8ff73ddf1c
Ecnalyr <ecnalyr@gmail.com>
2012-06-06 07:26:40 -0500

    Performed a small amount of refactoring

--- PaulSchool/Scripts/FillCourseForm.js
+++ PaulSchool/Scripts/FillCourseForm.js
@@ -4,17 +4,17 @@
 /// <reference path="jquery-ui-timepicker-addon.js" />
 
 //
-$(document).ready(function () {
-    $("#SelectedCourse").change(function () {
+$(document).ready(function() {
+    $("#SelectedCourse").change(function() {
         // When the dropdown is changed on the Apply To Teach A Course page, 
         // a request for a PartialView that contains a pre-filled form is sent to the controller
         var strSelected = "";
-        $("#SelectedCourse option:selected").each(function () {
+        $("#SelectedCourse option:selected").each(function() {
             strSelected += $(this)[0].value;
         });
         var url = "/Course/PreFillCourse/?SelectedCourse=" + strSelected;
 
-        $.post(url, function (data) {
+        $.post(url, function(data) {
             // appends the PartialView that was sent by the controller to "divToAppend"
             $("#originalForm").remove();
             $('#divToAppend').html(data);
