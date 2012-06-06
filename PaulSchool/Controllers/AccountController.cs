830a7ed9c79ad2e34ac875dbe3557b49ee43162f
Ecnalyr <ecnalyr@gmail.com>
2012-06-06 16:41:34 -0500

    Fixed typo

--- PaulSchool/Controllers/AccountController.cs
+++ PaulSchool/Controllers/AccountController.cs
@@ -3,7 +3,12 @@
 //   Copyright (c) Lost Tie LLC.  All rights reserved.
 //   This code and information is provided "as is" without warranty of any kind, either expressed or implied, including but not limited to the implied warranties of merchantability and fitness for a particular purpose.
 // </copyright>
+// <summary>
+//   The account controller.
+// </summary>
 // --------------------------------------------------------------------------------------------------------------------
+
+ // <summary>
 
 namespace PaulSchool.Controllers
 {
