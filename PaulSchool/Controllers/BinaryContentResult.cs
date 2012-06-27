93996d9369f9b329b8638a7e7e9161dc25c3990c
Ecnalyr <ecnalyr@gmail.com>
2012-06-27 09:34:29 -0500

    Added foundation for generating PDF certificates

---
+++ PaulSchool/Controllers/BinaryContentResult.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace PaulSchool.Controllers
+{
+    /// <summary>
+    /// An ActionResult used to send binary data to the browser.
+    /// </summary>
+    public class BinaryContentResult : ActionResult
+    {
+        private string ContentType;
+        private byte[] ContentBytes;
+
+        public BinaryContentResult(byte[] contentBytes, string contentType)
+        {
+            this.ContentBytes = contentBytes;
+            this.ContentType = contentType;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.Clear();
+            response.Cache.SetCacheability(HttpCacheability.NoCache);
+            response.ContentType = this.ContentType;
+
+            var stream = new MemoryStream(this.ContentBytes);
+            stream.WriteTo(response.OutputStream);
+            stream.Dispose();
+        }
+    }
+}
---
+++ PaulSchool/Controllers/CertificateController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaulSchool.Models;
+
+namespace PaulSchool.Controllers
+{
+    [HandleError]
+    public class CertificateController : PdfController
+    {
+        private readonly SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Certificate/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OrdersInHtml()
+        {
+            Student c = (from stud in db.Students where stud.StudentID == 1 select stud).First();
+            return View(c);
+        }
+
+        public ActionResult OrdersInPdf()
+        {
+            Student c = (from stud in db.Students where stud.StudentID == 1 select stud).First();
+            // To render a PDF instead of an HTML, all we need to do is call ViewPdf instead of View. This
+            // requires the controller to be inherited from MyController instead of MVC's Controller.
+            return ViewPdf(c);
+        }
+
+    }
+}
