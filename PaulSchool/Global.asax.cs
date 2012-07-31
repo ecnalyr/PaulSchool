dbc0e906a5f3eb3bef4e8ec3543445d29efc16c5
Ecnalyr <ecnalyr@gmail.com>
2012-07-31 08:01:33 -0500

    Added Custom Error Logging (NLog), fixed an error in _Layout

--- PaulSchool/Global.asax.cs
+++ PaulSchool/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Data.Entity;
 using System.Web.Security;
+using NLog;
 using PaulSchool.DAL;
 using PaulSchool.Models;
 
@@ -17,6 +19,42 @@ namespace PaulSchool
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+        }
+
+        protected void Application_Error()
+        {
+            var ex = Context.Server.GetLastError();
+
+            var logger = LogManager.GetLogger(GetType().ToString());
+
+            if (ex is HttpException && ((HttpException)ex).GetHttpCode() == 404)
+            {
+                if (logger.IsWarnEnabled)
+                {
+                    var message =
+                        String.Format(
+                            "Unhandled exception trying to access " + Context.Request.Url +
+                            ": {0} | Stack Trace: {1}",
+                            ex.Message,
+                            ex.StackTrace);
+
+                    logger.Warn(message, ex);
+                }
+            }
+            else
+            {
+                if (logger.IsFatalEnabled)
+                {
+                    var message =
+                        String.Format(
+                            "Unhandled exception trying to access " + Context.Request.Url +
+                            ": {0} | Stack Trace: {1}",
+                            ex.Message,
+                            ex.StackTrace);
+
+                    logger.Fatal(message, ex);
+                }
+            }
         }
 
         public static void RegisterRoutes(RouteCollection routes)
